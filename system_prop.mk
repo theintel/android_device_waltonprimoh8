fd1b1ed6b11f316365e732a632199ab136d4e0ea
Muhammad Arifur Rahman <theintelligentboy@ymail.com>
2022-04-28 15:52:31 +0600

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -1,32 +1,15 @@
 #
-# Copyright 2019 The Android Open Source Project
-# Copyright (C) 2019 The LineageOS Project
-# Copyright (C) 2013-2019 OmniROM
+# ADDITIONAL_DEFAULT_PROPERTIES
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+ro.secure=0
+security.perf_harden=0
+ro.adb.secure=0
+ro.allow.mock.location=1
+ro.debuggable=1
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# BOOTIMAGE_BUILD_PROPERTIES
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# System properties for Primo_H8, get these values through ADB or you can use 'adb shell getprop >> getprop.txt' command to get every prop value.
-
-ro.product.brand=WALTON
-ro.product.name=Primo_H8
-ro.product.device=Primo_H8
-ro.build.product=Primo_H8
-ro.treble.enabled=true
-persist.sys.usb.config=none
-persist.service.adb.enable=0
-persist.service.debuggable=0
-ro.secure=1
-ro.adb.secure=1
-ro.debuggable=0
-ro.allow.mock.location=0
+ro.bootimage.build.date=Sat Jan 12 18:34:14 CST 2019
+ro.bootimage.build.date.utc=1547289254
+ro.bootimage.build.fingerprint=WALTON/Primo_H8/Primo_H8:8.1.0/O11019/1547289254:user/release-keys
+persist.sys.usb.config=adb,mtp
